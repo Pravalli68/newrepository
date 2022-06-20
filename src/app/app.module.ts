07d23a3a6a62f166a4b00cb94e68a8ebf75a182f
unknown <kpravallika9963@gmail.com>
2022-06-20 20:48:34 +0530

    Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DistanceComponent } from './distance/distance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DistanceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
